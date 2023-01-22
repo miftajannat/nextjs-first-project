b24e2aab60729b6e2756468bbded646c72fdb5d9
miftajannat <miftajannat.dev@gmail.com>
2023-01-22 17:58:39 +0600

    header 2nd commit

--- components/header.js
+++ components/header.js
@@ -1,19 +1,21 @@
+import { ImFacebook, ImTwitter, ImYoutube } from "react-icons/im";
+
 function header() {
   return (
     <>
       <header className='bg-gray-50'>
-        <div className='lg:container lg:mx-auto flex'>
-          <div>
-            <input type='text' placeholder='Search' />
+        <div className='lg:container lg:mx-auto flex flex-col items-center sm:flex-row sm:justify-between text-center py-3'>
+          <div className="md:flex-none w-96 order-2 sm:order-1 flex justify-center py-4 sm:py-0 ">
+            <input type='text' className="mt-1 w-60 px-3 py-2 bg-white border border-slate 300 rounded-full text-sm shadow-sm placeholder-slate-500 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500" placeholder='Search' />
           </div>
-          <div>
-            <a> Design</a>
+          <div className="shrink w-80 sm:order-2">
+            <a className="font-bold uppercase text-3xl "> Design</a>
           </div>
-          <div>
-            <div className='flex'>
-              <a>Facebook</a>
-              <a>Twitter</a>
-              <a>Youtube</a>
+          <div className="w-96 order-3 flex justify-center">
+            <div className='flex gap-6'>
+              <a><ImFacebook color="#888888"/></a>
+              <a><ImTwitter color="#888888"/></a> 
+              <a><ImYoutube color="#888888"/></a>
             </div>
           </div>
         </div>
