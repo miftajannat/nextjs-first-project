0e4a70b44ccd1dc06ad3f58b5ab7e58ec6bb96d0
miftajannat <miftajannat.dev@gmail.com>
2023-01-23 10:39:08 +0600

    footer added

--- components/header.js
+++ components/header.js
@@ -1,4 +1,6 @@
-import { ImFacebook, ImTwitter, ImYoutube } from "react-icons/im";
+import { ImFacebook, ImTwitter, ImYoutube } from "react-icons/im"; 
+import Link from 'next/link';
+
 
 function header() {
   return (
@@ -9,13 +11,17 @@ function header() {
             <input type='text' className="mt-1 w-60 px-3 py-2 bg-white border border-slate 300 rounded-full text-sm shadow-sm placeholder-slate-500 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500" placeholder='Search' />
           </div>
           <div className="shrink w-80 sm:order-2">
-            <a className="font-bold uppercase text-3xl "> Design</a>
+            
+          <Link href={"/"}>  <a className="font-bold uppercase text-3xl "> Design</a></Link>
+          
+       
+          
           </div>
           <div className="w-96 order-3 flex justify-center">
             <div className='flex gap-6'>
-              <a><ImFacebook color="#888888"/></a>
-              <a><ImTwitter color="#888888"/></a> 
-              <a><ImYoutube color="#888888"/></a>
+              <Link href={"/"}> <a><ImFacebook color="#888888"/></a> </Link>
+              <Link href={"/"}>  <a><ImTwitter color="#888888"/></a>  </Link>
+              <Link href={"/"}><a><ImYoutube color="#888888"/></a> </Link>
             </div>
           </div>
         </div>
