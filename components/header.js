e5dd5962202314f3f7a105428867ee1e86af9e5a
miftajannat <miftajannat.dev@gmail.com>
2023-01-23 14:44:59 +0600

    header update

--- components/header.js
+++ components/header.js
@@ -1,27 +1,38 @@
-import { ImFacebook, ImTwitter, ImYoutube } from "react-icons/im"; 
-import Link from 'next/link';
-
+import { ImFacebook, ImTwitter, ImYoutube } from "react-icons/im";
+import Link from "next/link";
 
 function header() {
   return (
     <>
       <header className='bg-gray-50'>
         <div className='lg:container lg:mx-auto flex flex-col items-center sm:flex-row sm:justify-between text-center py-3'>
-          <div className="md:flex-none w-96 order-2 sm:order-1 flex justify-center py-4 sm:py-0 ">
-            <input type='text' className="mt-1 w-60 px-3 py-2 bg-white border border-slate 300 rounded-full text-sm shadow-sm placeholder-slate-500 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500" placeholder='Search' />
+          <div className='md:flex-none w-96 order-2 sm:order-1 flex justify-center py-4 sm:py-0 '>
+            <input
+              type='text'
+              className='mt-1 w-60 px-3 py-2 bg-white border border-slate 300 rounded-full text-sm shadow-sm placeholder-slate-500 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500'
+              placeholder='Search'
+            />
           </div>
-          <div className="shrink w-80 sm:order-2">
-            
-          <Link href={"/"}>  <a className="font-bold uppercase text-3xl "> Design</a></Link>
-          
-       
-          
+          <div className='shrink w-80 sm:order-2'>
+           
+              <a  href={"/"} className='font-bold uppercase text-3xl '> Design</a>
+   
           </div>
-          <div className="w-96 order-3 flex justify-center">
+          <div className='w-96 order-3 flex justify-center'>
             <div className='flex gap-6'>
-              <Link href={"/"}> <a><ImFacebook color="#888888"/></a> </Link>
-              <Link href={"/"}>  <a><ImTwitter color="#888888"/></a>  </Link>
-              <Link href={"/"}><a><ImYoutube color="#888888"/></a> </Link>
+             
+                <a href={"/"}>
+                  <ImFacebook color='#888888' />
+                </a>
+          
+        
+                <a href={"/"}>
+                  <ImTwitter color='#888888' />
+                </a>
+                         <a href={"/"}>
+                  <ImYoutube color='#888888' />
+                </a>
+     
             </div>
           </div>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
-import Header from '@/components/header'
+import { Inter } from "@next/font/google";
+import Header from "@/components/header";
 
-const inter = Inter({ subsets: ['latin'] })
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-<Header/>
+      <Header />
     </>
-  )
+  );
 }
