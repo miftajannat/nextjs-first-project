56b680195f1d211e4dc9f1e031be55d0ed08e140
miftajannat <miftajannat.dev@gmail.com>
2023-01-23 15:20:28 +0600

    section1added

---
+++ components/section1.js
@@ -0,0 +1,10 @@
+
+
+
+export default function section1() {
+    return (
+      <section >
+        Section1
+      </section>
+    )
+  }
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
-import { Inter } from "@next/font/google";
-import Header from "@/components/header";
+
+import Format from "@/layout/format"; 
 
 
-const inter = Inter({ subsets: ["latin"] });
+//components
+import Section1 from "@/components/section1";
+
 
 export default function Home() {
   return (
-    <>
-      <Header />
-    </>
+    <Format>
+      <Section1/>
+    </Format>
   );
 }
